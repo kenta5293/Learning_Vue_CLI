f1495c49cf72881bd3be8b0aafe80af3c25845be
kenta5293 <kenta.03.1110@gmail.com>
2021-04-11 18:40:32 +0900

    Learning Emit ☆

---
+++ src/components/InputField.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="form-group">
+    <label for>Name</label>
+    <!-- input이벤트를 실행할 때마다 updateName 메소드가 실행된다.  -->
+    <input type="text" :value="value" @input="$emit('input', $event.target.value)" />
+    <!-- <input type="text" name="name" :value="name" @input="updateName" /> -->
+    <!-- <input type="text" name="name" v-bind:value="name" v-on:input="updateName" /> -->
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      // name: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    updateName(e) {
+      console.log(e.target.value);
+      // $emit('eventName', value)
+      // eventName의 이름으로 value 값을 담아서 부모 컴포넌트에 보낼 수 있다.
+      // 즉 update-name이라는 이벤트에 e.target.value를 담는다는 의미인듯.
+      this.$emit("update-name", e.target.value);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.form > .form-group {
+  padding: 12px;
+}
+.form > .form-group > label {
+  font-size: 12px;
+  margin: 0 10px 0 0;
+}
+.form > .form-group > input {
+  font-size: 12px;
+  padding: 10px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,25 +7,39 @@ views 폴더에는 페이지 컴포넌트를 작성하고, 그 외의 작은 컴
 -->
 
 <template>
-  <div>
+  <div class="home">
     <h1>This is Home page</h1>
-    <Kenta />
+    <!-- <Kenta /> -->
+    <form action class="form">
+      <!-- <InputField :name="name" @update-name="updateName" /> -->
+      <InputField v-model="name" />
+      <button>Submit</button>
+    </form>
+
+    <br />
+    {{ name }}
   </div>
 </template>
 
 <script>
 // import를 하고 경로에 @를 적는 이유는 @는 src를 의미하기 때문이다.
-import Kenta from "@/components/Kenta.vue";
+// import Kenta from "@/components/Kenta.vue";
+import InputField from "@/components/InputField.vue";
 
 export default {
   components: {
-    Kenta
+    InputField
   },
   data() {
     // 여기서 data를 작성할 때 Function으로 작성하고, Object를 리턴하여 사용한다.
     return {
-      name: "Kenta"
+      name: ""
     };
+  },
+  methods: {
+    updateName(name) {
+      this.name = name;
+    }
   }
 };
 </script>
@@ -36,8 +50,19 @@ export default {
 /* index 전체에 여기 작성된 CSS가 적용되므로, 보통은 scoped를 작성한다. */
 /* scoped를 작성하면, 현재 이 컴포넌트 즉, Home.vue에만 적용된다. */
 /* 컴포넌트 별로 CSS를 관리할 수 있다는 장점이 존재한다. */
-
 h1 {
   color: red;
+}
+.form {
+  display: inline-flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 30px 16px;
+  margin: 12px auto;
+  border: 1px solid #525252;
+  font-size: 10px;
+}
+.form > button {
+  display: inline;
 }
 </style>
