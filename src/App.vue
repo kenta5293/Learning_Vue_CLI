3283150683a5c723a91ab2b7781802cb71dfb486
kenta5293 <kenta.03.1110@gmail.com>
2021-04-11 16:46:53 +0900

    Learning Vue-Router

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
     <div id="nav">
+      <!-- <a /> 대신 <router-link /> 이용하여 페이지 이동을 할 수 있다. -->
+      <!-- 또, to 라는 속성에 route에서 작성한 path 값을 넣어야 한다. -->
+      <!-- 브라우저에서는 <a />로 바뀌게 된다. -->
       <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>
     </div>
--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,18 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 
+// Vue.js에서는 vue-router를 이용해서, 화면을 이동하게 해준다.
+// 처음 열리는 html은 'public/index.html' 하나로, 자바스크립트를 이용하여 컴포넌트를 바꾸면서,
+// 페이지가 바뀌는 것 처럼 보인다. 이를 '싱글 페이지 어플리케이션' 즉 "SPA" 라고 한다.
+
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    // Object 안에는 3개의 속성이 있음.
+    path: '/',  // path는 URL에 들어가는 주소.
+    name: 'Home',  // name은 중복되지 않게 정해주면 된다.
+    component: Home  // Component에는 이름대로 해당 주소로 이동했을 때 보일 Component를 지정해주면 된다.
   },
   {
     path: '/about',
