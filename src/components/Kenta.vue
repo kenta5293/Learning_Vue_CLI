b002ce899935c3d634a295056e7681a3e7dca3ac
kenta5293 <kenta.03.1110@gmail.com>
2021-04-13 00:10:33 +0900

    Learning v-slot & Slot

--- src/components/Kenta.vue
+++ src/components/Kenta.vue
@@ -1,8 +1,29 @@
 <template>
   <div>
-    <h1>{{ title }}</h1>
+    <!-- emit -->
+    <!-- <h1>{{ title }}</h1>
+    <slot></slot>
     <p>{{ name }}</p>
-    <button @click="updateName">Update Name</button>
+    <slot></slot>
+    <button @click="updateName">Update Name</button>-->
+
+    <!-- slot -->
+    <!-- 
+      컴포넌트 사이에 다른 코드를 작성하고 싶은 경우에는 "slot" 이라는 것을 추가하면 된다.
+      <slot>을 컴포넌트에 작성하고, 컴포넌트 태그를 열어서 그 안에 HTML 코드를 작성하면,
+      그 코드가 <slot>에 들어가게 된다.
+
+      또, <slot>은 여러번 작성이 가능한데, 이를 구분 할 때에는 name 속성을 추가해야 한다.
+      ( name 속성이 없을 경우 default가 들어가는 듯. )
+
+      slot에 데이터를 보내줄 수 도 있다. v-bind:(name)="(data)" 이런 형식으로, 
+      (data)를 (name)이라는 이름으로 slot에 보내어, 뷰 페이지에서 사용이 가능하다.
+    -->
+    <p>Header</p>
+    <slot name="header" :uploadKenta="kenta"></slot>
+    <p>Body</p>
+    <slot></slot>
+    <p>Footer</p>
   </div>
 </template>
 
@@ -18,13 +39,18 @@ export default {
     // title 이라는 이름의 props.
     title: {
       type: String, // title의 데이터타입이 String 이어야 한다는 뜻. 다른 타입일 경우 경고를 보냄.
-      default: "Default Title", // 만약 title을 보내지 않았을 경우의 default 값.
-      required: false // required를 true로 하면 반드시 보내줘야 하고, false일 경우 반드시 보낼 필요는 없다.
+      default: "Default Title" // 만약 title을 보내지 않았을 경우의 default 값.
+      // required: false // required를 true로 하면 반드시 보내줘야 하고, false일 경우 반드시 보낼 필요는 없다.
+    },
+    name: {
+      type: String,
+      default: "Default Name"
     }
   },
   data() {
     return {
-      name: "Kenta"
+      // name: "Kenta"
+      kenta: "Web-Kenta"
     };
   },
   methods: {
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,32 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <Kenta />
+    <Kenta>
+      <!-- 
+        2개 이상의 <slot>을 사용할 때에는 <template> 태그를 이용한다.
+        여기에 v-slot: 이라는 속성을 이용하여, name으로 구분한 slot을 설정해줄 수 있다.
+        즉, <template v-slot:header>는 name이 header인 <slot>을 의미하고, 이 템플릿에 작성한
+        HTML코드는 컴포넌트의 <slot name="header"> 이 곳에 들어가게 된다.
+
+        slot을 통해 받아온 데이터를 이용하고 싶을 때에는 템플릿에 v-slot:(slotName)="slotProps"
+        이렇게 작성하고, 템플릿 태그 안에는 {{ slotProps.dataName }} 이렇게 작성하면 된다.
+        여기서 slotProps는 임의의 이름으로 작성해준다.
+
+        v-slot은 v-bind를 : 으로 대체하여 쓸 수 있는 것과 같이 #으로 대체가능하다.
+        [ v-slot:header는 #header로 대체가능 ]
+      -->
+
+      <!-- <template v-slot:header="props"> -->
+      <template v-slot:header="{ uploadKenta }">
+        <p>No.1 | Header</p>
+        <!-- <p>No.2 | {{ props.uploadKenta }}</p> -->
+        <p>No.2 | {{ uploadKenta }}</p>
+      </template>
+
+      <template #default>
+        <p>Body Content</p>
+      </template>
+    </Kenta>
   </div>
 </template>
 
